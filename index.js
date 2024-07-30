d8d0608f67be5e40aa00e6bae93ca03afb71943a
Sabobeh1 <146986675+Sabobeh1@users.noreply.github.com>
2024-07-30 16:42:08 +0300

    Swiper with Api

--- index.js
+++ index.js
@@ -1,28 +1,4 @@
-const swiper = new Swiper('.swiper', {
-    
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    autoplay: {
-        delay: 1000
-    },
 
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
-});
 
 
 async function getApi(){
@@ -32,12 +8,45 @@ async function getApi(){
     let response = await feh.json();
     
     let data = response.products;
-    console.log(products);
     
-    let printDiv = document.querySelector('.swiper-wrapper');
+  
 
+    let htmlContent = data.map(function(item){
+        return `
+        <div class="swiper-slide"><img src="${item.thumbnail}"></img></div>`;
+     }).join('')
+
+     let printDiv = document.querySelector('.swiper-wrapper');
+     printDiv.innerHTML = htmlContent; // Update the table content
+
+     const swiper = new Swiper('.swiper', {
     
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+       
+    
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+    
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
 
 }
+
+
+
+
 
 getApi();
