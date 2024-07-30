e7e45460f0d9345178414fa4aa8fc560e1af77fb
Sabobeh1 <146986675+Sabobeh1@users.noreply.github.com>
2024-07-30 12:13:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 
 //================================= form with table using array of objects===================//
-saf
-
-
+let arr = [1,2,3,4,5,6]
+let shakir = arr.map(function(number){
+    return `<h3>${number}</h3>`;
+}).join('')
+let sh = document.querySelector('body')
+console.log(shakir)
+sh.innerHTML=shakir;
 
